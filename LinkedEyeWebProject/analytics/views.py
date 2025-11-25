86c3e475b77f35fa09b8a56ead858009b702eee9
Rajkumar-Ashokan <rajkumar.ashokan@finspot.in>
2025-11-25 13:04:26 +0530

    Update views.py

--- LinkedEyeWebProject/analytics/views.py
+++ LinkedEyeWebProject/analytics/views.py
@@ -302,9 +302,16 @@ def monitorgraph(request):
 
 def search_elasticsearch(request):
     try:
-        index_name = 'noren-login-history'
+        #index_name = 'noren-login-history'
         # print('elastic host--->{}'.format((request.GET.get("elastic_host", "172.20.1.80"))))
         # print('elastic port--->{}'.format(request.GET.get("elastic_port", 31545)))
+        # Read subsite and index_name from JS
+        subsite = request.GET.get("subsite", "")
+        index_name = request.GET.get("index_name", "noren-login-history")
+
+        print("Subsite received:", subsite)
+        print("Index Name received:", index_name)
+
         es = Elasticsearch(
             [{'host': (request.GET.get("elastic_host", "172.20.1.80")), 
                 'port': int(request.GET.get("elastic_port", 31545)),
