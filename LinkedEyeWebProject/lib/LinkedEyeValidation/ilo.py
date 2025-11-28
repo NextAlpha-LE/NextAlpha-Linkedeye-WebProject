d5c432c27bb4b74b1931aece46548001da73f214
Rajkumar-Ashokan <rajkumar.ashokan@finspot.in>
2025-11-28 12:14:03 +0530

    Update ilo.py

--- LinkedEyeWebProject/lib/LinkedEyeValidation/ilo.py
+++ LinkedEyeWebProject/lib/LinkedEyeValidation/ilo.py
@@ -1,9 +1,12 @@
 import os
 import redfish
+import urllib3
+
+# Disable SSL warnings for self-signed certificates
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class ilo():
     def __init__(self, ip=None, username=None, password=None):
-        #print("ilo-ip--->"+str(ip))
         self.ip = ip
         self.url = "https://{}".format(self.ip)
         self.username = username
@@ -11,15 +14,27 @@ class ilo():
 
     def check(self):
         try:
-            #print("ilo--check-->")
-            REST_OBJ = redfish.redfish_client(base_url=self.url, username=self.username, password=self.password, timeout=2, max_retry=1)
+            # Increased timeout and retries
+            REST_OBJ = redfish.redfish_client(
+                base_url=self.url, 
+                username=self.username, 
+                password=self.password, 
+                timeout=10,      # Increased from 2
+                max_retry=3      # Increased from 1
+            )
+            
             REST_OBJ.login(auth="session")
             response = REST_OBJ.get('/redfish/v1/systems/1')
+            
             if '20' in str(response.status):
-                #print("ilo--check-1-->")
                 return True
             else:
                 return False
+                
         except Exception as ex:
-            #print("ilo-check-2---?")
             return False
+        finally:
+            try:
+                REST_OBJ.logout()
+            except:
+                pass
