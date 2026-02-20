3fa6a46c2b772c60893d93aab430f54e954d3e3b
Rajkumar-Ashokan <rajkumar.ashokan@finspot.in>
2026-02-20 15:54:45 +0530

    Update urls.py

--- LinkedEyeWebProject/bodeodstatus/urls.py
+++ LinkedEyeWebProject/bodeodstatus/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.bodeodstatus, name='BOD-EOD Status'),
     path('adp', views.adpstatus, name='ADP Status'),
     path('eod', views.eodstatus, name='EOD Status'),
+    path('le-adp-status', views.leadp_status, name='LE ADP Status'), #new LE_ADP site
     path('getbodeodkeys', views.getbodeodkeys, name='getbodeodkeys'),
     path('updatekeys', views.updatekeys, name='updatekeys'),
     path('readfile', views.read_file, name='readfile')
