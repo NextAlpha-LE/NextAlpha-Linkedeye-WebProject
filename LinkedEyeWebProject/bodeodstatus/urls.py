373d39489b9176ec19b7557fa354020636fb1488
Rajkumar-Ashokan <rajkumar.ashokan@finspot.in>
2026-04-16 11:02:48 +0530

    Update urls.py

--- LinkedEyeWebProject/bodeodstatus/urls.py
+++ LinkedEyeWebProject/bodeodstatus/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 from . import messagequeue_views as mq_views
+from . import bandwidth_views as bw_views
 
 app_name = "bod-eodstatus"
 #print('bod-eodstatus----------->')
@@ -14,8 +15,7 @@ urlpatterns = [
     path('updatekeys', views.updatekeys, name='updatekeys'),
     path('readfile', views.read_file, name='readfile'),
 
-    # MessageQueue (analytics DB)
-    # MessageQueue (analytics DB)
+    # MessageQueue (LinkedEye DB)
     path('messagequeue', mq_views.messagequeue_dashboard, name='messagequeue_dashboard'),
     path('messagequeue-data', mq_views.messagequeue_data, name='messagequeue_data'),
     path('messagequeue-stats', mq_views.messagequeue_stats, name='messagequeue_stats'),
@@ -25,4 +25,10 @@ urlpatterns = [
     path('messagequeue-queue-line1',   mq_views.get_queue_line1,   name='get_queue_line1'),
     path('messagequeue-queue-line2',   mq_views.get_queue_line2,   name='get_queue_line2'),
     path('messagequeue-sites',           mq_views.messagequeue_sites, name='messagequeue_sites'),
+
+    # Bandwidth (LinkedEye DB)
+    path('bandwidth-summary', bw_views.get_bandwidth_summary, name='bandwidth_summary'),
+    path('bandwidth-trends',  bw_views.get_bandwidth_trends,  name='bandwidth_trends'),
+    path('bandwidth-status',  bw_views.get_interface_status,  name='bandwidth_status'),
+    path('bandwidth-sync',    bw_views.sync_prometheus_bandwidth, name='bandwidth_sync'),
 ]
