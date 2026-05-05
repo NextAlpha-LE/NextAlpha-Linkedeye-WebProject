bfb18e5a039669d875cac3be632b20eb3fe894c8
Rajkumar-Ashokan <rajkumar.ashokan@finspot.in>
2026-05-05 12:30:58 +0530

    Update urls.py

--- LinkedEyeWebProject/incidents/urls.py
+++ LinkedEyeWebProject/incidents/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('api/incidents/', views.get_incidents_api, name='incidents_api_slash'),
     path('api/chart-data', views.get_incidents_chart_data, name='incidents_chart_data'),
     path('api/chart-data/', views.get_incidents_chart_data, name='incidents_chart_data_slash'),
+    path('api/per-site', views.get_incidents_per_site, name='incidents_per_site'),
+    path('api/per-site/', views.get_incidents_per_site, name='incidents_per_site_slash'),
     path('create/', views.create_incident, name='create_incident'),
     path('<str:incident_id>/', views.incident_detail, name='incident_detail'),
 ]
