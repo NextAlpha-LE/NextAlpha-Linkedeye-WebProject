ec53db5345bf715a70331b8181fec77d07b59cac
Rajkumar-Ashokan <rajkumar.ashokan@finspot.in>
2026-07-06 14:54:43 +0530

    Update models.py

--- LinkedEyeWebProject/userprofile/models.py
+++ LinkedEyeWebProject/userprofile/models.py
@@ -1,34 +1,33 @@
 from django.db import models
-from django.contrib.auth.models import Group, User
-import json  # Add this line to import the json module
+from django.contrib.auth.models import User
 from lesites.models import SiteModel
 
-# Create your models here.
 
 class policynotifiModel(models.Model):
-   policy_id = models.AutoField(primary_key=True)
-   subject_category = models.CharField(max_length=50)
-   device_type = models.CharField(max_length=50)
-   device_friendly_name = models.CharField(max_length=255)
-   device_ip = models.CharField(max_length=100)
-   categories = models.TextField()  # Hardware or Hardware,Software,Ping
-   escalation_mails = models.TextField()
-   definite_mails = models.TextField()
-   approval_timer = models.IntegerField(default=0)
-   resolution_timer = models.IntegerField(default=0)
-   escalation_required = models.BooleanField(default=True)
-   is_enabled = models.BooleanField(default=True)
-   created_at = models.DateTimeField(auto_now_add=True)
-   updated_at = models.DateTimeField(auto_now=True)
+    policy_id = models.AutoField(primary_key=True)
+    subject_category = models.CharField(max_length=50)
+    device_type = models.CharField(max_length=50)
+    device_friendly_name = models.CharField(max_length=255)
+    device_ip = models.CharField(max_length=100)
+    categories = models.TextField()  # Hardware or Hardware,Software,Ping
+    escalation_mails = models.TextField()
+    definite_mails = models.TextField()
+    approval_timer = models.IntegerField(default=0)
+    resolution_timer = models.IntegerField(default=0)
+    escalation_required = models.BooleanField(default=True)
+    is_enabled = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-   class Meta:
-       db_table= "policy"
+    class Meta:
+        db_table = "policy"
+
 
 class subsiteModel(models.Model):
-   id = models.AutoField(primary_key=True)
-   site = models.ForeignKey(SiteModel, on_delete=models.CASCADE)
-   user = models.ForeignKey(User, on_delete=models.CASCADE)
-   sub_site = models.TextField()
+    id = models.AutoField(primary_key=True)
+    site = models.ForeignKey(SiteModel, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sub_site = models.TextField()
 
-   class Meta:
-       db_table= "subsite"
+    class Meta:
+        db_table = "subsite"
